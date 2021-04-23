5e1b4f8b53f93e23d3c32bb056f8765848f42066
Michael Kness <mpkness@gmail.com>
2021-04-23 10:03:52 -0700

    add netlify cms plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
